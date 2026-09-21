c0326dca8c798f9d27eae949ec61898c82c8ddbe
DAVA30 <diegovespa04@gmail.com>
2026-09-20 21:07:46 -0400

    Verificacion_Entorno

---
+++ verificacion_entorno.py
@@ -0,0 +1,9 @@
+print("================================================================")
+
+Nombre= "Diego Vespa"
+Edad= "22 años"
+Fecha_actual= "20-09-2026"
+
+print(f"Mi entorno está listo. Soy {Nombre}, y tengo {Edad}. La fecha actual es {Fecha_actual}")
+
+print("================================================================")
